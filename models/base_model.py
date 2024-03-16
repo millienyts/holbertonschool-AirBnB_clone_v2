761bd817be598ef0b298095eea2714dcbb7b469a
Felipe Santos <felipecassiors@gmail.com>
2024-03-16 15:30:27 -0400

    console.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,53 +1,69 @@
 #!/usr/bin/python3
-""" Base Model Module """
+"""
+BaseModel for HBNB project.
+"""
+
 import models
-from datetime import datetime
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
-import uuid
+from sqlalchemy.ext.declarative import declarative_base
+from uuid import uuid4
+from datetime import datetime
 
 Base = declarative_base()
 
 
 class BaseModel:
-    """ Defines all common attributes/methods for other classes """
+    """
+    The BaseModel class from which future classes will be derived.
+    """
 
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    id = Column(String(60), primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
-        """ Initializes the BaseModel """
-
-        self.id = str(uuid.uuid4())
+        """
+        Initialization of the base model.
+        """
+        self.id = str(uuid4())
         self.created_at = self.updated_at = datetime.utcnow()
-
         if kwargs:
-            for k, v in kwargs.items():
-                if k != "__class__":
-                    setattr(self, k, v)
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+            if 'created_at' in kwargs:
+                self.created_at = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if 'updated_at' in kwargs:
+                self.updated_at = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+
+    def __str__(self):
+        """
+        String representation of the BaseModel class.
+        """
+        return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.to_dict())
 
     def save(self):
-        """ Updates the updated_at attribute and saves it to the storage """
-
+        """
+        Updates the attribute 'updated_at' with the current datetime.
+        """
         self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
-    def delete(self):
-        """ Deletes the current instance from storage """
-
-        models.storage.delete(self)
-
     def to_dict(self):
-        """ Returns a dictionary containing all keys/values of __dict__ """
+        """
+        Returns a dictionary containing all keys/values of __dict__ of the instance.
+        """
+        my_dict = self.__dict__.copy()
+        my_dict['__class__'] = self.__class__.__name__
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in my_dict:
+            del my_dict['_sa_instance_state']
+        return my_dict
 
-        new_dict = dict(self.__dict__)
-        new_dict['__class__'] = self.__class__.__name__
-        new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['updated_at'] = self.updated_at.isoformat()
-
-        if "_sa_instance_state" in new_dict:
-            del new_dict["_sa_instance_state"]
-
-        return new_dict
+    def delete(self):
+        """
+        Deletes the current instance from the storage (models.storage).
+        """
+        models.storage.delete(self)
