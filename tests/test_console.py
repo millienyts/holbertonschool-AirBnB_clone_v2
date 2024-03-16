7ef8e23d1de0bf1fe6ebd2283c81410c959471bb
Felipe Santos <felipecassiors@gmail.com>
2024-03-16 12:46:55 -0400

    console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -2,6 +2,7 @@
 """Test suite for the console."""
 
 import sys
+import os
 from io import StringIO
 import unittest
 from unittest.mock import patch
@@ -23,18 +24,21 @@ class TestConsole(unittest.TestCase):
         """Helper method to create a console instance."""
         return HBNBCommand()
 
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'file', "skip if not FileStorage")
     @patch('models.storage')
     def test_quit(self, mock_storage):
         """Test if 'quit' command exists."""
         console = self.create_console()
         self.assertTrue(console.onecmd("quit"))
 
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'file', "skip if not FileStorage")
     @patch('models.storage')
     def test_EOF(self, mock_storage):
         """Test if 'EOF' command exists."""
         console = self.create_console()
         self.assertTrue(console.onecmd("EOF"))
 
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'file', "skip if not FileStorage")
     @patch('models.storage')
     def test_all(self, mock_storage):
         """Test 'all' command output type."""
@@ -42,6 +46,7 @@ class TestConsole(unittest.TestCase):
         console.onecmd("all")
         self.assertIsInstance(self.capturedOutput.getvalue(), str)
 
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'file', "skip if not FileStorage")
     @patch('models.storage')
     def test_create_show_flow(self, mock_storage):
         """Test 'create' and 'show' command flow."""
@@ -56,6 +61,7 @@ class TestConsole(unittest.TestCase):
         console.onecmd(f"show User {user_id}")
         self.assertTrue(isinstance(self.capturedOutput.getvalue(), str))
 
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'file', "skip if not FileStorage")
     @patch('models.storage')
     def test_error_messages(self, mock_storage):
         """Test various error messages for 'show' and 'create'."""
