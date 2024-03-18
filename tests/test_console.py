847464ba8fe0b4e1aa1e57bb862623c4803fe551
Felipe Santos <felipecassiors@gmail.com>
2024-03-18 15:51:43 -0400

    console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -104,7 +104,7 @@ class TestCreateCommand(unittest.TestCase):
 
     def tearDown(self):
         """Clean up resources after tests."""
-        # Implement cleanup logic if necessary, like removing created objects from storage
+        # Implement cleanup logic if necessary, removing created objects from storage
 
 
 class TestConsoleDocs(unittest.TestCase):
