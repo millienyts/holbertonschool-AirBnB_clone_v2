dbae6c85021ed135374216bf0ed23375b0d93a4f
Felipe Santos <felipecassiors@gmail.com>
2024-03-18 01:13:02 -0400

    console.py

--- console.py
+++ console.py
@@ -11,6 +11,7 @@ from models.amenity import Amenity
 from models.review import Review
 import pycodestyle
 
+
 class HBNBCommand(cmd.Cmd):
     """ Contains the functionality for the HBNB console"""
 
@@ -71,8 +72,7 @@ class HBNBCommand(cmd.Cmd):
                 # if arguments exist beyond _id
                 pline = pline[2].strip()  # pline is now str
                 if pline:
-                    # check for *args or **kwargs
-                    if pline[0]  is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is'}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -339,6 +339,6 @@ class HBNBCommand(cmd.Cmd):
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
