b0f8b850d1376fe29ccfe67c483a04f6d8e8df81
Felipe Santos <felipecassiors@gmail.com>
2024-03-18 16:05:15 -0400

    console.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -23,27 +23,28 @@ class FileStorage:
             temp = {k: v.to_dict() for k, v in self.__objects.items()}
             json.dump(temp, f)
 
-    def reload(self):
-        """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it's inside"""
+        if obj is not None:
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        if key in FileStorage.__objects:
+            del FileStorage.__objects[key]
 
+       def reload(self):
+        """Reloads the JSON file to __objects"""
+        from models.base_model import BaseModel
+        # Import other classes as needed
+        
         classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
+            "BaseModel": BaseModel,
+            # Add other classes here
         }
-        try:
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for k, v in temp.items():
-                    cls_name = v['__class__']
-                    if cls_name in classes:
-                        self.__objects[k] = classes[cls_name](**v)
-        except FileNotFoundError:
-            pass
+    try:
+            with open(FileStorage.__file_path, "r") as f:
+             objects = json.load(f)
+             for obj_id, obj_dict in objects.items():
+                cls_name = obj_dict["__class__"]
+                if cls_name in classes:
+                   self.__objects[obj_id] = classes[cls_name](**obj_dict)
+    except (FileNotFoundError, json.JSONDecodeError):
+    pass
