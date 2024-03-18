c7d46d580cb49b9213d13796861454b06542a540
Felipe Santos <felipecassiors@gmail.com>
2024-03-18 16:09:06 -0400

    console.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,14 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
+    def all(self, cls=None):
+        """
+        Returns a dictionary of models currently in storage
+        Optionally, returns objects of a specific class
+        """
+        if cls:
+            class_name = cls.__name__ if type(cls) == type else cls
+            return {k: v for k, v in FileStorage.__objects.items() if k.split('.')[0] == class_name}
         return FileStorage.__objects
 
     def new(self, obj):
@@ -26,25 +32,36 @@ class FileStorage:
     def delete(self, obj=None):
         """Deletes obj from __objects if it's inside"""
         if obj is not None:
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        if key in FileStorage.__objects:
-            del FileStorage.__objects[key]
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            if key in FileStorage.__objects:
+                del FileStorage.__objects[key]
 
-       def reload(self):
+    def reload(self):
         """Reloads the JSON file to __objects"""
         from models.base_model import BaseModel
-        # Import other classes as needed
-        
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
         classes = {
             "BaseModel": BaseModel,
-            # Add other classes here
+            "User": User,
+            "Place": Place,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Review": Review
         }
-    try:
+
+        try:
             with open(FileStorage.__file_path, "r") as f:
-             objects = json.load(f)
-             for obj_id, obj_dict in objects.items():
-                cls_name = obj_dict["__class__"]
-                if cls_name in classes:
-                   self.__objects[obj_id] = classes[cls_name](**obj_dict)
-    except (FileNotFoundError, json.JSONDecodeError):
-    pass
+                objects = json.load(f)
+                for obj_id, obj_dict in objects.items():
+                    cls_name = obj_dict["__class__"]
+                    if cls_name in classes:
+                        self.__objects[obj_id] = classes[cls_name](**obj_dict)
+        except (FileNotFoundError, json.JSONDecodeError):
+            pass
