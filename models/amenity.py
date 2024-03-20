853d408ce7f96297ef3c1b4b46b179c86b78ed34
Felipe Santos <felipecassiors@gmail.com>
2024-03-20 07:39:15 -0400

    console.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" Amenity module for the HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ Amenity class to store amenity information """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship(
+        "Place", secondary=place_amenity, viewonly=False)
--- models/place.py
+++ models/place.py
@@ -3,8 +3,15 @@
 Place Module for HBNB project
 """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
+from os import getenv
+
+# Define the association table for a many-to-many relationship between Place and Amenity
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey(
+                          'places.id'), primary_key=True, nullable=False),
+                      Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -20,6 +27,44 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
+
     # Relationship with User
     user = relationship("User", back_populates="places")
-    # If amenities is needed, a secondary relationship can be defined here
+
+    # For DBStorage: Defines the many-to-many relationship between Place and Amenity
+    amenities = relationship("Amenity", secondary=place_amenity,
+                             back_populates="place_amenities", viewonly=False)
+
+    # For FileStorage: Define amenity_ids as a list of Amenity.id
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def amenities(self):
+            """Returns the list of Amenity instances from amenity_ids"""
+            from models import storage
+            from models.amenity import Amenity
+            amenity_list = []
+            for amenity_id in self.amenity_ids:
+                amenity = storage.all(Amenity).get(
+                    'Amenity.{}'.format(amenity_id))
+                if amenity:
+                    amenity_list.append(amenity)
+            return amenity_list
+
+        @amenities.setter
+        def amenities(self, obj):
+            """Adds an Amenity.id to the amenity_ids list"""
+            if type(obj).__name__ == 'Amenity':
+                if hasattr(self, 'amenity_ids'):
+                    self.amenity_ids.append(obj.id)
+                else:
+                    self.amenity_ids = [obj.id]
+
+# Ensure the relationship is set up in Amenity model too
+
+
+class Amenity(BaseModel, Base):
+    """ Amenity model """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship(
+        "Place", secondary=place_amenity, back_populates="amenities", viewonly=False)
