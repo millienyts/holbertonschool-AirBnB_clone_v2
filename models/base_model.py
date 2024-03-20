09455fa54132fa6ff76425d4fb25f178ea570cfa
Felipe Santos <felipecassiors@gmail.com>
2024-03-19 20:48:10 -0400

    console.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,79 +1,45 @@
 #!/usr/bin/python3
-"""
-This module defines a base class for all models in our hbnb clone
-"""
+"""This module defines a base class for all models in our hbnb clone"""
 import uuid
-from sqlalchemy import Column, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
-
-Base = declarative_base()
 
 
 class BaseModel:
-    """
-    A base class for all hbnb models with SQLAlchemy and FileStorage compatibility
-    """
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
-    updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    """A base class for all hbnb models"""
 
     def __init__(self, *args, **kwargs):
-        """
-        Instantiates a new model with potential SQLAlchemy and FileStorage compatibility
-        """
-        # Assign ID and timestamps if not provided in kwargs
-        self.id = kwargs.get('id', str(uuid.uuid4()))
-        self.created_at = kwargs.get('created_at', datetime.utcnow())
-        self.updated_at = kwargs.get('updated_at', datetime.utcnow())
-
-        # Apply the rest of the kwargs to instance attributes
-        for key, value in kwargs.items():
-            if key != "__class__":
-                setattr(self, key, value)
+        """Instatntiates a new model"""
+        if not kwargs:
+            from models import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def __str__(self):
-        """
-        Returns a string representation of the instance
-        """
-        cls_name = self.__class__.__name__
-        return '[{}] ({}) {}'.format(cls_name, self.id, self.to_dict())
+        """Returns a string representation of the instance"""
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
-        """
-        Updates updated_at with current time when instance is changed
-        """
+        """Updates updated_at with current time when instance is changed"""
         from models import storage
-        self.updated_at = datetime.utcnow()
-        storage.new(self)
+        self.updated_at = datetime.now()
         storage.save()
 
-    def delete(self):
-        """
-        Deletes the current instance from the storage
-        """
-        from models import storage
-        storage.delete(self)
-
     def to_dict(self):
-        """
-        Convert instance into dict format, including attributes for SQLAlchemy
-        """
-        dictionary = self.__dict__.copy()
-        dictionary['__class__'] = self.__class__.__name__
-        dictionary['created_at'] = dictionary['created_at'].isoformat()
-        dictionary['updated_at'] = dictionary['updated_at'].isoformat()
-
-        # Remove SQLAlchemy instance state, if present
-        dictionary.pop('_sa_instance_state', None)
-
+        """Convert instance into dict format"""
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        dictionary.update({'__class__':
+                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
-
-    @classmethod
-    def close(cls):
-        """
-        Call remove() method on the private session attribute (self.__session)
-        or close() on the class session if it's a session of the Session type
-        """
-        from models import storage
-        storage.close()
