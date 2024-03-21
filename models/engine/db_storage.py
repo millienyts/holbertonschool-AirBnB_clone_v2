1d8d34e9ae1e67e55f54409b2825840a8b2d481d
Felipe Santos <felipecassiors@gmail.com>
2024-03-21 11:55:32 -0400

    console.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from os import getenv
-from models.base_model import Base
+from models.base_model import Base, BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
@@ -26,20 +26,47 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        # Query and return all objects of class `cls` (if given) or all objects in DB
-        pass
+        """Returns dict of current database"""
+        db_dict = {}
+        classes = {
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
+        if cls:
+            for key in classes.keys():
+                if cls.__name__ == key:
+                    objects = (self.__session.query(classes[key]).all())
+                    obj_class = key
+                    break
+            for obj in objects:
+                id = obj.id
+                obj_key = '{}.{}'.format(obj_class, id)
+                db_dict[obj_key] = obj
+            return db_dict
+        all_objects = (self.__session.query(City, State, User,
+                                            Place, Review, Amenity).filter(
+            City.state_id == State.id,
+            Place.user_id == User.id,
+            Place.city_id == City.id,
+            Review.place_id == Place.id,
+            Review.user_id == User.id).all())
 
     def new(self, obj):
+        """Add the object to the current database session."""
         self.__session.add(obj)
 
     def save(self):
+        """Commit all changes of the current database session."""
         self.__session.commit()
 
     def delete(self, obj=None):
+        """Delete from the current database session obj if not None."""
         if obj:
             self.__session.delete(obj)
 
     def reload(self):
+        """Reload all tables in the database and create the session."""
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(
             bind=self.__engine, expire_on_commit=False)
