87686beb3f06a02cd2f392a46c70e3e04135c4a6
Felipe Santos <felipecassiors@gmail.com>
2024-03-21 12:03:44 -0400

    console.py

--- models/__init__.py
+++ models/__init__.py
@@ -5,13 +5,6 @@ depending on the environment variable HBNB_TYPE_STORAGE.
 """
 
 import os
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 
 # Import the declarative base for SQLAlchemy
 from sqlalchemy.ext.declarative import declarative_base
