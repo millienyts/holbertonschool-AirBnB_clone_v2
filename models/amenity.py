50cf0c9e8cac055ff7886fbd24a67e08e7dcf333
millienytorressanta <tmillieny@gmail.com>
2024-03-21 22:04:47 -0400

    console

--- models/amenity.py
+++ models/amenity.py
@@ -1,18 +1,24 @@
-# models/amenity.py
 from models.base_model import Base, BaseModel
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Table, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
-
 
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    # Use the string name for `secondary` to avoid direct imports and help with the resolution at query time.
-    place_amenities = relationship(
-        "Place", secondary=place_amenity, back_populates="amenities")
+    
+    # Define the association table between Amenity and Place
+    place_amenity = Table(
+        'place_amenity',
+        Base.metadata,
+        Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True),
+        Column('place_id', String(60), ForeignKey('places.id'), primary_key=True)
+    )
 
-    # DBStorage: Establish relationship only if using DBStorage
+    # Establish relationship only if using DBStorage
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        # Assuming 'place_amenity' 'place.py'
-        from models.place import place_amenity
+        places = relationship(
+            "Place",
+            secondary=place_amenity,
+            back_populates="amenities"
+        )
