af73e3044b9acf6f078af04849713561783260fa
millienytorressanta <tmillieny@gmail.com>
2024-03-21 23:02:15 -0400

    console

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,52 +1,61 @@
 #!/usr/bin/python3
-"""
-Unit tests for the State class
-"""
+"""test for state"""
 import unittest
+import os
 from models.state import State
 from models.base_model import BaseModel
 
 
 class TestState(unittest.TestCase):
-    """
-    Test cases for the State model class.
-    """
+    """this will test the State class"""
 
-    def setUp(self):
-        """
-        Simple setup before each test method.
-        """
-        self.state_instance = State()
+    @classmethod
+    def setUpClass(cls):
+        """set up for test"""
+        cls.state = State()
+        cls.state.name = "CA"
 
-    def test_inheritance(self):
-        """
-        Test if the instance is an instance of BaseModel and State
-        """
-        self.assertIsInstance(self.state_instance, State)
-        self.assertIsInstance(self.state_instance, BaseModel)
-
-    def test_attributes(self):
-        """
-        Test if State has the correct attributes and if they are set correctly
-        """
-        self.assertTrue(hasattr(State, "name"),
-                        "State class does not have attribute 'name'")
-        self.assertIsInstance(State.name, str)
-
-    def test_name_assignment(self):
-        """
-        Test assignment and type of 'name' attribute.
-        """
-        self.state_instance.name = "California"
-        self.assertEqual(self.state_instance.name, "California")
-        self.assertIsInstance(self.state_instance.name, str)
+    @classmethod
+    def teardown(cls):
+        """at the end of the test this will tear it down"""
+        del cls.state
 
     def tearDown(self):
-        """
-        Clean up actions after each test method.
-        """
-        del self.state_instance
+        """teardown"""
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
+
+    def test_checking_for_docstring_state(self):
+        """checking for docstrings"""
+        self.assertIsNotNone(State.__doc__)
+
+    def test_attributes_state(self):
+        """chekcing if State have attributes"""
+        self.assertTrue('id' in self.state.__dict__)
+        self.assertTrue('created_at' in self.state.__dict__)
+        self.assertTrue('updated_at' in self.state.__dict__)
+        self.assertTrue('name' in self.state.__dict__)
+
+    def test_is_subclass_state(self):
+        """test if State is subclass of BaseModel"""
+        self.assertTrue(issubclass(self.state.__class__, BaseModel), True)
+
+    def test_attribute_types_state(self):
+        """test attribute type for State"""
+        self.assertEqual(type(self.state.name), str)
+
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") == 'db', 'Not file engine')
+    def test_save_state(self):
+        """test if the save works"""
+        self.state.save()
+        self.assertNotEqual(self.state.created_at, self.state.updated_at)
+
+    def test_to_dict_state(self):
+        """test if dictionary works"""
+        self.assertEqual('to_dict' in dir(self.state), True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
