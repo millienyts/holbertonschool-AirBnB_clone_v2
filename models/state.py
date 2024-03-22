bf9e8f970b4a0bcba28e0323964926f8854c8370
millienytorressanta <tmillieny@gmail.com>
2024-03-21 22:22:09 -0400

    console

--- models/state.py
+++ models/state.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from models.base_model import BaseModel
+""" State Module """
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
-from os import getenv
-import models
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
+    name = ""
+class State(BaseModel, Base):
+    """ Defines the State class """
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        # For DBStorage: Establish a relationship to cities with cascade options
-        cities = relationship('City', backref='state',
-                              cascade='all, delete-orphan')
-    else:
-        # For FileStorage: Implement a getter property to fetch city instances
-        @property
-        def cities(self):
-            """Getter attribute cities for FileStorage"""
-            all_cities = models.storage.all(models.City)
-            # Filter cities by state_id
-            state_cities = [city for city in all_cities.values()
-                            if city.state_id == self.id]
-            return state_cities
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", back_populates="state")
