9e97f143be4c72e58176042979553b01ec92875b
millienytorressanta <tmillieny@gmail.com>
2024-03-21 23:11:38 -0400

    console

--- models/state.py
+++ models/state.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-""" State Module """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.city import City
+from os import getenv
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
 class State(BaseModel, Base):
-    """ Defines the State class """
-
-    __tablename__ = 'states'
-
+    """ State class """
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", back_populates="state")
+
+    cities = relationship("City", backref="state",
+                          cascade="all, delete-orphan")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.save()
+
+    @property
+    def cities(self):
+        """
+        Getter for cities related to a state using a FIlEStorage engine
+        """
+        from models import storage
+        st_cities = []
+        for city in storage.all(City).values():
+            if (self.id == city.state_id):
+                st_cities.append(city)
+        return st_cities
--- models/user.py
+++ models/user.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-"""
-This module defines a class User.
-"""
-from models.base_model import BaseModel, Base
+""" User Module for HBNB project """
+
 from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from os import getenv
 
 
 class User(BaseModel, Base):
+    '''
+    User class
+    '''
+
     __tablename__ = 'users'
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
 
-   # Relationships
     if getenv("HBNB_TYPE_STORAGE") == "db":
         places = relationship('Place', backref='user', cascade='all, delete')
         reviews = relationship("Review", backref='user', cascade="all, delete")
