4c17a9bccd7f70df716effdeaa65426c47e225dc
millienytorressanta <tmillieny@gmail.com>
2024-03-21 23:13:09 -0400

    console

--- models/__init__.py
+++ models/__init__.py
@@ -1,32 +1,12 @@
 #!/usr/bin/python3
-"""
-This module instantiates an object of class FileStorage or DBStorage
-depending on the environment variable HBNB_TYPE_STORAGE.
-"""
+"""create a unique FileStorage instance for your application"""
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from os import getenv
 
-import os
-
-# Import the declarative base for SQLAlchemy
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
-# Determine storage type based on environment variable
-HBNB_TYPE_STORAGE = os.getenv('HBNB_TYPE_STORAGE', 'file')
-
-if HBNB_TYPE_STORAGE == 'db':
-    # DBStorage specific imports
-    from models.engine.db_storage import DBStorage
+if getenv("HBNB_TYPE_STORAGE") == "db":
     storage = DBStorage()
+    # print(getenv("HBNB_TYPE_STORAGE"))
 else:
-    # FileStorage specific imports
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
-
-# Establish relationships for DBStorage, if necessary
-if HBNB_TYPE_STORAGE == 'db':
-    # Assuming the relationship table is defined here
-    from models.place import PlaceAmenity
-
-# Finalize and prepare for first use
 storage.reload()
