7626e824388ffe52cc101e60183c37828cde620b
millienytorressanta <tmillieny@gmail.com>
2024-04-09 13:31:32 -0400

    web flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,15 +1,16 @@
+#!/usr/bin/python3
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-
+# Route to display "Hello HBNB!"
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
 
-
 @app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
+def hbnb():
     return 'HBNB'
 
 
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,22 +1,22 @@
-from flask import Flask, escape
+#!/usr/bin/python3
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-
+# Route to display "Hello HBNB!"
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
 
-
 @app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
+def hbnb():
     return 'HBNB'
 
-
 @app.route('/c/<text>', strict_slashes=False)
-def display_c(text):
-    return 'C {}'.format(escape(text).replace('_', ' '))
-
+def c_display(text):    
+    text = text.replace('_', ' ')
+    return f"C {text}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
