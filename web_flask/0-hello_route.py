968f17915460452a7f2afdb807222d6a52af8fda
millienytorressanta <tmillieny@gmail.com>
2024-04-11 01:33:29 -0400

    flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,6 +8,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
+"""Starts a Flask web application"""
+
 from flask import Flask
-from flask import render_template
 
 app = Flask(__name__)
 
-# Route to display "Hello HBNB!"
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
+def display_hbnb():
+    """Displays 'HBNB'"""
     return 'HBNB'
 
 
