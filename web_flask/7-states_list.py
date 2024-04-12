99da44afd264d1f692368cebb5c3e48c66ad9d8c
millienytorressanta <tmillieny@gmail.com>
2024-04-12 18:15:09 -0400

    SQL

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,14 +1,95 @@
+#!/usr/bin/python3
+'''
+    Flask app
+'''
+from flask import Flask
+from flask import render_template
 from models import storage
-from flask import Flask, render_template
+from models.state import State
 
 app = Flask(__name__)
 
+# Route to display "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    '''
+        Prints Hello HBNB!
+    '''
+    return 'Hello HBNB!'
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    states = storage.all("State").values()
+# Route to display "HBNB"
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''
+        Prints HBNB
+    '''
+    return 'HBNB'
+
+# Route to display text that starts with 'c'
+@app.route('/c/<text>', strict_slashes=False)
+def c_display(text="is cool"):
+    '''
+        Prints c <text>
+    '''
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+# Both routes apply to the same method
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_display(text="is cool"):
+    '''
+        Prints python <text>/ is cool
+    '''
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+# Route to display a number
+@app.route('/number/<int:n>', strict_slashes=False)
+def int_display(n):
+    '''
+        prints number <n>
+    '''
+    # Check if n is an integer
+    if isinstance(n, int):
+        return f'{n} is a number'
+    else:
+        return '404 not found'
+
+# Route to display a number and render a template
+@app.route('/number_template/<int:n>')
+def display_number(n):
+    '''
+        Renders template and also number <n> is sent
+    '''
+    if isinstance(n, int):
+        return render_template('5-number.html', number=n)
+
+# Route to display if a number is odd or even
+@app.route('/number_odd_or_even/<int:n>')
+def display_even_odd(n):
+    # Check if n is an integer
+    if isinstance(n, int):
+        n = int(n)
+        n_type = 'odd'
+        # Determine if the number is even or odd
+        if n % 2 == 0:
+            n_type = 'even'
+        return render_template('6-number_odd_or_even.html', number=n,
+                                number_type=n_type)
+
+# Route to display a list of states
+@app.route('/states_list')
+def display_states():
+    '''
+        Displays all states with id
+    '''
+    # Get all State objects
+    states = storage.all(State)
+    # Sort states alphabetically by name
+    states = sorted(states.values(), key=lambda state: state.name)
+    # Render template and pass states to it
     return render_template('7-states_list.html', states=states)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,14 +1,73 @@
+#!/usr/bin/python3
+import sys
+sys.path.append('c:\\https://github.com/millienyts/holbertonschool-AirBnB_clone_v2.git')
+from flask import Flask
+from flask import render_template
 from models import storage
-from flask import Flask, render_template
+from models.state import State
+from models.city import City
 
 app = Flask(__name__)
 
+# Route to display "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_display(text="is cool"):    
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+# Both routes apply to the same method
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_display(text="is cool"): 
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+@app.route('/number/<n>', strict_slashes=False)
+def int_display(n):
+    if n.isdigit():
+        return f'{n} is a number'
+    else:
+        return '404 not found'
+
+@app.route('/number_template/<n>', strict_slashes=False)
+def display_number(n):
+    if n.isdigit():
+        return(render_template('5-number.html', number=n))
+    else:
+        return '404 not found'
+
+@app.route('/number_odd_or_even/<n>', strict_slashes=False)
+def display_even_odd(n):
+    if n.isdigit():
+        n = int(n)
+        n_type = 'odd'
+        if n % 2 == 0:
+            n_type = 'even'
+        return(render_template('6-number_odd_or_even.html', number=n, number_type=n_type))
+    else:
+        return '404 not found'
+
+@app.route('/states_list', strict_slashes=False)
+def display_states():
+    states = storage.all(State)
+    # Get the objects from {state_id: {objects}} and order by name A -Z
+    states = sorted(states.values(), key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
 
 @app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    states = storage.all("State").values()
+def display_cities_states():
+    states = storage.all(State)
+    # Get the objects from {state_id: {objects}} and order by name A -Z
+    states = sorted(states.values(), key=lambda state: state.name)
     return render_template('8-cities_by_states.html', states=states)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
